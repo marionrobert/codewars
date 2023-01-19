b5d904ef35372f72cc85c34b01a2aa094e386fc9
Marion Robert <mrobert148@gmail.com>
2023-01-19 15:43:23 +0100

    adding better solutions for find_outlier(integers) method

--- python/6kyu/find_outlier/main.py
+++ python/6kyu/find_outlier/main.py
@@ -18,3 +18,14 @@ def find_outlier(integers):
       if n % 2 == 0:
         return n
 
+
+# better solutions
+def find_outlier(int):
+    odds = [x for x in int if x%2!=0]
+    evens= [x for x in int if x%2==0]
+    return odds[0] if len(odds)<len(evens) else evens[0]
+
+#
+def find_outlier(integers):
+    parity = [n % 2 for n in integers]
+    return integers[parity.index(1)] if sum(parity) == 1 else integers[parity.index(0)]
