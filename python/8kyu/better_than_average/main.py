72bf6fce5c9825a2a9408ab1362008783cd5aaec
Marion Robert <mrobert148@gmail.com>
2023-03-27 17:22:28 +0200

    test

--- python/8kyu/better_than_average/main.py
+++ python/8kyu/better_than_average/main.py
@@ -6,3 +6,5 @@ def better_than_average(class_points, your_points):
 # better solution
 def better_than_average(class_points, your_points):
     return your_points > sum(class_points) / len(class_points)
+
+# commented lines to test
